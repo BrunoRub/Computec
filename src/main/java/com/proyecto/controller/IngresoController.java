1cde4ea4de3facca2c01c86a38c613b1b6282227
BrunoRub <brunorrubio@gmail.com>
2021-11-11 20:43:16 -0500

    Implementacion de los Controller de Ingreso y Pedido

--- src/main/java/com/proyecto/controller/IngresoController.java
+++ src/main/java/com/proyecto/controller/IngresoController.java
@@ -1,5 +1,56 @@
 package com.proyecto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.entity.Ingreso;
+import com.proyecto.service.IngresoService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("ingreso")
 public class IngresoController {
+	
+	public Ingreso ingreso;
+	
+	@Autowired 
+	private IngresoService service;
+	
+	@GetMapping()
+	public List<Ingreso>listar(){
+		return service.listar();
+	}
+	
+	@GetMapping("{iding}")
+	public Ingreso IngresoId(@PathVariable int iding){
+		return service.obtenerPorId(iding).get();
+	}
+	
+	@PostMapping
+	public Ingreso registrar(Ingreso ingreso){
+		return service.registrar(ingreso);
+	}
+	
+	@PutMapping("{iding}")
+	public ResponseEntity<Object>actualizar(@RequestBody Ingreso ingreso, @PathVariable int iding){
+		ingreso.setIding(iding);
+		service.actualziar(ingreso);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("{iding}")
+	public void eliminar(@PathVariable int iding) {
+		service.eliminar(iding);
+	}
 
 }
--- src/main/java/com/proyecto/controller/ProveedorController.java
+++ src/main/java/com/proyecto/controller/ProveedorController.java
@@ -1,5 +1,55 @@
 package com.proyecto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.entity.Proveedor;
+import com.proyecto.service.ProveedorService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("proveedor")
 public class ProveedorController {
 
+	public Proveedor proveedor;
+	
+	@Autowired
+	private ProveedorService service;
+	
+	@GetMapping
+	public List<Proveedor>listar(){
+		return service.listar();
+	}
+	
+	@GetMapping("{idprov}")
+	public Proveedor ProveedorId(@PathVariable int idprov){
+		return service.obtenerPorId(idprov).get();
+	}
+	
+	@PostMapping
+	public Proveedor registrar(Proveedor proveedor){
+		return service.registrar(proveedor);
+	}
+	
+	@PutMapping("{idprov}")
+	public ResponseEntity<Object>actualizar(@RequestBody Proveedor proveedor, @PathVariable int idprov){
+		proveedor.setIdprov(idprov);
+		service.actualizar(proveedor);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("{idprov}")
+	public void eliminar(@PathVariable int idprov) {
+		service.eliminar(idprov);
+	}
 }
