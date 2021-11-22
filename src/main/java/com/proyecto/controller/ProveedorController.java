d72d3fd0bac4941e870c6b68c4bbbb948e3979f8
bruno <bruno@DESKTOP-IPLR7MD>
2021-11-22 16:21:43 -0500

    Crud Proveedor

--- src/main/java/com/proyecto/controller/ProveedorController.java
+++ src/main/java/com/proyecto/controller/ProveedorController.java
@@ -4,28 +4,34 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyecto.entity.Ingreso;
 import com.proyecto.entity.Proveedor;
 import com.proyecto.service.ProveedorService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
-@RestController
-@RequestMapping("proveedor")
+//@RestController
+//@RequestMapping("proveedor")
+@Controller
 public class ProveedorController {
 
-	public Proveedor proveedor;
+	//public Proveedor proveedor;
 	
 	@Autowired
 	private ProveedorService service;
 	
+	/*
 	@GetMapping("/lista")
 	public List<Proveedor>listar(){
 		return service.listar();
@@ -51,5 +57,42 @@ public class ProveedorController {
 	@DeleteMapping("{idprov}")
 	public void eliminar(@PathVariable Long idprov) {
 		service.eliminar(idprov);
+	}
+	*/
+	
+	@GetMapping("/proveedor")
+	public String listar(Model map){
+		map.addAttribute("listarProveedor", service.listar());
+		return "proveedor/listar";
+	}
+	
+	@GetMapping("/proveedor/nuevo")
+	public String nuevo(Model map){
+		map.addAttribute("proveedor", new Proveedor());
+		return "proveedor/nuevo";
+	}
+	
+	@PostMapping("/proveedor/crear")
+	public String crear(@ModelAttribute("proveedor") Proveedor proveedor){
+		service.registrar(proveedor);
+		return "redirect:/proveedor";
+	}
+	
+	@GetMapping("/proveedor/editar/{idprov}")
+	public String editar(@ModelAttribute("idprov") Long idprov,Model map){
+		map.addAttribute("proveedor", service.obtenerPorId(idprov));
+		return "proveedor/editar";
+	}
+	
+	@PostMapping("/proveedor/actualizar")
+	public String actualizar(@ModelAttribute("proveedor") Proveedor proveedor){
+		service.actualizar(proveedor);
+		return "redirect:/proveedor";
+	}
+	
+	@GetMapping("/proveedor/eliminar/{idprov}")
+	public String eliminar(@ModelAttribute("idprov") Long idprov){
+		service.eliminar(idprov);
+		return "redirect:/proveedor";
 	}
 }
